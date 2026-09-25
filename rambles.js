66d1c20bc4bef299a00b2ae8f0901ffa144e0ee5
Kirsten Chapman <chapkirsten@gmail.com>
2026-09-25 17:45:40 -0600

    update

--- rambles.js
+++ rambles.js
@@ -9,7 +9,7 @@
 const RAMBLES = [
   {
   "date": "25.09.26",
-  "title": "DUTCH DREAMS",
+  "title": "DUTCH DREAMS.",
   "category": "ON REPEAT",
   "preview": "One song. Several questionable decisions.",
   "body": "<p>I have a song taking over the airwaves of my brain. <em>Nightclub</em> by Balthazar. Backstory time: I found this song around the same time that I got my driver’s license.</p> <p>Following my personal pattern of making decisions based on musical interests, I even tried (unsuccessfully) to learn Dutch to feel more connected with Patricia, Jinte, Martin, and others.</p> <p>Anyway, that’s it. That’s the post.</p> <p>Just sharing my love of <em>Nightclub</em>.</p> <p>I’m dun dun-dun dun, dun dun-dun dun….</p>\n<div class=\"entry-scrawl\">\n   Tot ziens →\n  </div>"
